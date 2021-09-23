9fa648ec7666898f4e3a0fc0fc0b64f4eac5f125
Ameth1208 <Amethgm@gmail.com>
2021-09-23 08:07:10 -0500

    Ameth

---
+++ AsignaSalones.App.Persistencia/AppRepositorios/IRepositorioDirectivo.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using AsignaSalones.App.Dominio;
+
+namespaceAsignaSalones.App.Persistencia
+{
+    public interface IRepositorioDirectivo
+    {
+        //GetAllDirectivos
+        IEnumerable<Directivo> GetAllDirectivos();
+        //AddDirectivo
+        Directivo AddDirectivo(Directivo Directivo);
+        //UpdateDirectivo
+        Directivo UpdateDirectivo(Directivo Directivo);
+        //DeleteDirectivo
+        void DeleteDirectivo(int idDirectivo);
+        //GetDirectivo
+        Directivo GetDirectivo(int idDirectivo);
+
+    }
+}
---
+++ AsignaSalones.App.Persistencia/AppRepositorios/RepositorioDirectivo.cs
@@ -0,0 +1,60 @@
+using AsignaSalones.App.Dominio;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AsignaSalones.App.Persistencia
+{
+    public class RepositorioDirectivo : IRepositorioDirectivo
+    {
+        private readonly AppContext _appContext;
+        public RepositorioDirectivo(AppContext appContext)
+        {
+            _appContext = appContext;
+        }
+
+        Directivo IRepositorioDirectivo.AddDirectivo(Directivo directivo)
+        {
+            var directivoAdicionado = _appContext.directivos.Add(directivo) ;
+            _appContext.SaveChanges();
+            return directivoAdicionado.Entity ;
+        }
+
+        Directivo IRepositorioDirectivo.Updatedirectivo(directivo directivo)
+        {
+            var directivoEncontrado = _appContext.directivos.FirstOrDefault(p => p.id == directivo.id);
+            if (directivoEncontrado != null)
+            {
+                directivoEncontrado.nombre = directivo.nombre;
+                directivoEncontrado.apellidos = directivo.apellidos;
+                directivoEncontrado.identificacion = directivo.identificacion;
+                directivoEncontrado.telefono = directivo.telefono;
+                directivoEncontrado.correo = directivo.correo;
+                directivoEncontrado.vehiculo1 = directivo.vehiculo1;
+                directivoEncontrado.vehiculo2 = directivo.vehiculo2;
+                
+                _appContext.SaveChanges();
+            }
+            return directivoEncontrado;
+        }
+
+        void IRepositorioDirectivo.Deletedirectivo(int iddirectivo)
+        {
+            var directivoEncontrado = _appContext.directivos.FirstOrDefault(p => p.id == iddirectivo);
+            if (directivoEncontrado == null)
+                return;
+            _appContext.directivos.Remove(directivoEncontrado);
+            _appContext.SaveChanges();
+        }
+
+        Directivo IRepositorioDirectivo.Getdirectivo(int iddirectivo)
+        {
+            var directivoEncontrado = _appContext.directivos.FirstOrDefault(p => p.id == iddirectivo);
+            return directivoEncontrado;
+        }
+
+         IEnumerable<directivo> IRepositorioDirectivo.GetAlldirectivo()
+        {
+            return _appContext.directivos;
+        }
+    }
+}
